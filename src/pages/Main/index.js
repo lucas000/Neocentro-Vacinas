bec89b16c16e49e92e58157e90686654385022e0
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-07-12 23:00:44 -0300

    Update layout page main with new design

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Container,
   TextMain,
   ButtonSchedule,
+  WelcomeContainer,
+  ContainerAvatar,
+  UserAvatar,
+  LogoCompany,
+  CompanyName,
   TextButtonSchedule,
 } from './styles';
 
+import logo from '../../assets/logo.png';
 import {useNavigation} from '@react-navigation/native';
 
 const Main = () => {
+  const [user, setUser] = useState('Lucas');
+
   const navigation = useNavigation();
 
   function handleScheduleVaccine() {
-    console.log('Tocou');
     navigation.navigate('SearchCity');
   }
 
   return (
     <Container>
-      <TextMain>Você deseja realizar uma vacina?</TextMain>
+      <WelcomeContainer>
+        <TextMain>Bem vindo, {user}</TextMain>
+
+        <ContainerAvatar>
+          <UserAvatar source={logo} />
+        </ContainerAvatar>
+      </WelcomeContainer>
 
       <ButtonSchedule onPress={handleScheduleVaccine}>
         <TextButtonSchedule>Agendar uma vacina</TextButtonSchedule>
       </ButtonSchedule>
+
+      <LogoCompany source={logo} />
+      <CompanyName>Neocentro Vacinas</CompanyName>
     </Container>
   );
 };
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,27 +3,60 @@ import {RectButton} from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: #ffffff;
+`;
+
+export const WelcomeContainer = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  background-color: #008789;
+  align-content: center;
 `;
 
 export const TextMain = styled.Text`
   font-size: 20px;
-  font-weight: bold;
-  color: #f7f7f7;
+  font-family: 'Roboto-Medium';
+  color: #000000;
+  margin-left: 50px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 48px;
+  height: 48px;
+  margin-left: 80px;
+  border-radius: 22px;
+`;
+export const ContainerAvatar = styled.View`
+  margin-left: 20px;
 `;
 
 export const ButtonSchedule = styled(RectButton)`
   width: 80%;
   height: 48px;
-  background: #f7f7f7;
+  background: #1abc9c;
+  margin-left: 40px;
+  margin-top: 180px;
   border-radius: 5px;
   align-items: center;
   justify-content: center;
-  margin-top: 60px;
 `;
 
 export const TextButtonSchedule = styled.Text`
-  font-size: 16px;
+  font-size: 18px;
+  color: #f7f7f7;
+  font-family: 'Roboto-Regular';
+`;
+
+export const LogoCompany = styled.Image`
+  width: 200px;
+  height: 200px;
+  margin-left: 100px;
+`;
+export const CompanyName = styled.Text`
+  font-size: 22px;
+  margin-top: 30px;
+  margin-left: 95px;
+  color: #1abc9c;
+  font-family: 'Roboto-Regular';
 `;
